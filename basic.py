ef16ad776385fd9a99e5c195d26c031454476148
Alexander Shpilkin <ashpilkin@gmail.com>
2019-05-07 15:34:04 +0300

    Move plotting into a function

--- basic.py
+++ basic.py
@@ -11,12 +11,13 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--npz', default='https://github.com/schitaytesami/lab/releases/download/data/electionsData.npz')
-parser.add_argument('--weights', default='voters', choices=['voters', 'given', 'leader', 'ones'], help='''  'ones'    (counts polling stations);   'voters'  (counts registered voters);  'given'   (counts given ballots);  'leader'  (counts ballots for the leader) ''')
+parser.add_argument('--weights', default='voters', choices={'voters', 'given', 'leader', 'ones'}, help='''  'ones'    (counts polling stations);   'voters'  (counts registered voters);  'given'   (counts given ballots);  'leader'  (counts ballots for the leader) ''')
 parser.add_argument('--min-size', default=0, type=int)
 parser.add_argument('--noise', action='store_true', help='Add U(-0.5,0.5) noise to the numerators (to remove division artifacts)')
 parser.add_argument('--bin-width', default=0.25, type=float, help='Bin width in percentage points')
 parser.add_argument('--seed', default=1, type=int, help='Seed for random noise')
 parser.add_argument('--colormap', default='viridis', type=str)
+parser.add_argument('--dpi', default=None, type=int)
 args = parser.parse_args()
 
 def urlopen(url):
@@ -79,78 +80,81 @@ def histogram(data, binwidth, weights='voters', minsize=0, noise=False):
                      bins=edges, weights=wval[ind])[0]
   return wlbl, centers, h
 
+def plot(title, wlbl, centers, h, cmap='viridis'):
+  binwidth=centers[1] - centers[0]
+  ht = np.sum(h, axis=1)
+  hr = np.sum(h, axis=0)
+
+  ymax = min(np.max(ht), np.max(hr))
+  ylog, yfac = 0, 1
+  while yfac < ymax:
+    ylog, yfac = ylog+1, yfac*10
+  ylog, yfac = ylog-1, yfac//10
+
+  size, aspect, spacing = 9.0, 3, 0.15
+
+  fig, axs = plt.subplots(2, 2, sharex='col', sharey='row', figsize=[size, size], gridspec_kw=dict(width_ratios=[aspect, 1], wspace=spacing, height_ratios=[1, aspect], hspace=spacing))
+  fig.suptitle(title, size=20, y=0.925, va='baseline')
+
+  ax = axs[0,1]
+  ax.text(0.5, 0.5, f'$\\times 10^{{{ylog}}}$ {wlbl}\nin ${binwidth}\\,\\%$ bin', wrap=True, ha='center', va='center', transform=ax.transAxes)  # the \n is a hack to force good wrapping
+  ax.set_frame_on(False)
+  ax.axhline(0, 0, 1, color='black')
+  ax.axvline(0, 0, 1, color='black')
+  ax.tick_params(right=False, top=False, left=True, bottom=True,
+                 labelright=False, labeltop=False, labelleft=True, labelbottom=True)
+
+  ax = axs[0,0]
+  ax.plot(centers, ht / yfac, linewidth=1, color=plt.get_cmap(cmap)(0))
+  ax.set_xticks(np.arange(0, 101, 10))
+  ax.set_ylim(0, ax.get_ylim()[1])
+  ax.set_frame_on(False)
+  ax.axhline(0, 0, 1, color='black')
+  ax.axvline(100, 0, 1, color='black')
+  ax.tick_params(right=True, top=False, left=False, bottom=True,
+                 labelright=False, labeltop=False, labelleft=False, labelbottom=True)
+
+  ax = axs[1,1]
+  ax.plot(hr / yfac, centers, linewidth=1, color=plt.get_cmap(cmap)(0))
+  ax.set_xlim(0, ax.get_xlim()[1])
+  ax.set_yticks(np.arange(0, 101, 10))
+  ax.set_frame_on(False)
+  ax.axhline(100, 0, 1, color='black')
+  ax.axvline(0, 0, 1, color='black')
+  ax.tick_params(right=False, top=True, left=True, bottom=False,
+                 labelright=False, labeltop=False, labelleft=True, labelbottom=False)
+
+  ax = axs[1,0]
+  ax.imshow(h.T, vmin=0, vmax=np.quantile(h, 0.99), origin='lower', extent=[0,100,0,100], cmap=cmap, interpolation='none')
+  ax.set_xlabel('Turnout %')
+  ax.set_ylabel('Leader’s result %')
+  ax.set_frame_on(False)
+  ax.axhline(100, 0, 1, color='black')
+  ax.axvline(100, 0, 1, color='black')
+  ax.tick_params(right=True, top=True, left=False, bottom=False,
+                 labelright=False, labeltop=False, labelleft=False, labelbottom=False)
+
+  top    = min(axs[0,0].get_position().y0, axs[0,0].get_position().y1)
+  bottom = max(axs[1,0].get_position().y0, axs[1,0].get_position().y1)
+  offset = ((top - bottom) / axs[0,0].get_position().height / 2 -
+            axs[0,0].xaxis.get_major_ticks()[0].get_tick_padding() / 144)
+  for tick in axs[0,0].xaxis.get_major_ticks() + axs[0,1].xaxis.get_major_ticks():
+    tick.label1.set_va('center')
+    tick.set_pad(0)
+    tick.label1.set_position((tick.label1.get_position()[0], -offset))
+
+  right = min(axs[1,1].get_position().x0, axs[1,1].get_position().x1)
+  left  = max(axs[1,0].get_position().x0, axs[1,0].get_position().x1)
+  offset = ((right - left) / axs[1,1].get_position().width / 2 -
+            axs[1,1].yaxis.get_major_ticks()[0].get_tick_padding() / 144)
+  for tick in axs[0,1].yaxis.get_major_ticks() + axs[1,1].yaxis.get_major_ticks():
+    tick.label1.set_ha('center')
+    tick.set_pad(0)
+    tick.label1.set_position((-offset, tick.label1.get_position()[1]))
+
 year = 2018
 D = loadnpz(args.npz, year)
 wlbl, centers, h = histogram(D, args.bin_width, weights=args.weights, minsize=args.min_size, noise=args.noise)
-ht = np.sum(h, axis=1)
-hr = np.sum(h, axis=0)
-
-ymax = min(np.max(ht), np.max(hr))
-ylog, yfac = 0, 1
-while yfac < ymax:
-  ylog, yfac = ylog+1, yfac*10
-ylog, yfac = ylog-1, yfac//10
-
-size, aspect, spacing = 9.0, 3, 0.15
-
-fig, axs = plt.subplots(2, 2, sharex='col', sharey='row', figsize=[size, size], gridspec_kw=dict(width_ratios=[aspect, 1], wspace=spacing, height_ratios=[1, aspect], hspace=spacing))
-fig.suptitle(f'Russian election {year}', size=20, y=0.925, va='baseline')
-
-ax = axs[0,1]
-ax.text(0.5, 0.5, f'$\\times 10^{{{ylog}}}$ {wlbl}\nin ${args.bin_width}\\,\\%$ bin', wrap=True, ha='center', va='center', transform=ax.transAxes)  # the \n is a hack to force good wrapping
-ax.set_frame_on(False)
-ax.axhline(0, 0, 1, color='black')
-ax.axvline(0, 0, 1, color='black')
-ax.tick_params(right=False, top=False, left=True, bottom=True,
-               labelright=False, labeltop=False, labelleft=True, labelbottom=True)
-
-ax = axs[0,0]
-ax.plot(centers, ht / yfac, linewidth=1, color=plt.get_cmap(args.colormap)(0))
-ax.set_xticks(np.arange(0, 101, 10))
-ax.set_ylim(0, ax.get_ylim()[1])
-ax.set_frame_on(False)
-ax.axhline(0, 0, 1, color='black')
-ax.axvline(100, 0, 1, color='black')
-ax.tick_params(right=True, top=False, left=False, bottom=True,
-               labelright=False, labeltop=False, labelleft=False, labelbottom=True)
-
-ax = axs[1,1]
-ax.plot(hr / yfac, centers, linewidth=1, color=plt.get_cmap(args.colormap)(0))
-ax.set_xlim(0, ax.get_xlim()[1])
-ax.set_yticks(np.arange(0, 101, 10))
-ax.set_frame_on(False)
-ax.axhline(100, 0, 1, color='black')
-ax.axvline(0, 0, 1, color='black')
-ax.tick_params(right=False, top=True, left=True, bottom=False,
-               labelright=False, labeltop=False, labelleft=True, labelbottom=False)
-
-ax = axs[1,0]
-ax.imshow(h.T, vmin=0, vmax=np.quantile(h, 0.99), origin='lower', extent=[0,100,0,100], cmap=args.colormap, interpolation='none')
-ax.set_xlabel('Turnout %')
-ax.set_ylabel('Leader’s result %')
-ax.set_frame_on(False)
-ax.axhline(100, 0, 1, color='black')
-ax.axvline(100, 0, 1, color='black')
-ax.tick_params(right=True, top=True, left=False, bottom=False,
-               labelright=False, labeltop=False, labelleft=False, labelbottom=False)
-
-top    = min(axs[0,0].get_position().y0, axs[0,0].get_position().y1)
-bottom = max(axs[1,0].get_position().y0, axs[1,0].get_position().y1)
-offset = ((top - bottom) / axs[0,0].get_position().height / 2 -
-          axs[0,0].xaxis.get_major_ticks()[0].get_tick_padding() / 144)
-for tick in axs[0,0].xaxis.get_major_ticks() + axs[0,1].xaxis.get_major_ticks():
-  tick.label1.set_va('center')
-  tick.set_pad(0)
-  tick.label1.set_position((tick.label1.get_position()[0], -offset))
-
-right = min(axs[1,1].get_position().x0, axs[1,1].get_position().x1)
-left  = max(axs[1,0].get_position().x0, axs[1,0].get_position().x1)
-offset = ((right - left) / axs[1,1].get_position().width / 2 -
-          axs[1,1].yaxis.get_major_ticks()[0].get_tick_padding() / 144)
-for tick in axs[0,1].yaxis.get_major_ticks() + axs[1,1].yaxis.get_major_ticks():
-  tick.label1.set_ha('center')
-  tick.set_pad(0)
-  tick.label1.set_position((-offset, tick.label1.get_position()[1]))
-
-plt.savefig('basic.png', bbox_inches='tight')
+plot(f'Russian election {year}', wlbl, centers, h, cmap=args.colormap)
+plt.savefig('basic.png', bbox_inches='tight', dpi=args.dpi)
 plt.close()
