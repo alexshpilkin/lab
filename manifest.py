dbcb4df680577ea1b16f35afc333ae34591ed336
Alexander Shpilkin <ashpilkin@gmail.com>
2019-05-08 00:59:21 +0300

    Implement a simple RFC 822 tokenizer

---
+++ manifest.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+from email.parser import Parser
+
+class strview(object):
+	__slots__ = ('_string', '_start', '_stop')
+
+	def __init__(self, string, start=0, stop=None):
+		self._string = str(string)
+		self._start  = min(start, len(self._string))
+		self._stop   = (min(stop, len(self._string))
+		                if stop is not None
+		                else len(self._string))
+
+	def __getitem__(self, index):
+		if not isinstance(index, slice):
+			return self._string[self._start + index.__index__()]
+		if index.step is not None:
+			raise ValueError('stepping is not supported')
+		start = index.start if index.start is not None else 0
+		if start < 0 or index.stop is not None and index.stop < 0:
+			raise ValueError('negative indices are not supported')
+		return type(self)(self._string,
+		                  self._start + start,
+		                  self._start + index.stop
+		                  if index.stop is not None
+		                  else self._stop)
+
+	def __str__(self):
+		return self._string[self._start:self._stop]
+
+	def __len__(self):
+		return (min(self._stop, len(self._string)) -
+		        min(self._start, len(self._string)))
+
+LONERS = frozenset('"(),/:;<=>?@[\]{}')
+SPACES = frozenset(' \t\r\n()')
+
+def trim(s):
+	i, n = 0, 0
+	while i < len(s):
+		if s[i] == '(':
+			n += 1
+		elif s[i] == ')':
+			n -= 1
+			if n < 0:
+				raise ValueError('uninitiated comment')
+		elif s[i] == '\\':
+			i += 1
+			if i >= len(s):
+				raise ValueError('unterminated quoted-pair')
+		elif n == 0 and s[i] not in SPACES:
+			break
+		i += 1
+	else:
+		if n > 0:
+			raise ValueError('unterminated comment')
+	return str(s[:i]), s[i:]
+
+def chop(s):
+	assert s
+	if s[0] == '"':
+		i, b = 1, []
+		while i < len(s):
+			if s[i] == '"':
+				return (''.join(b), *trim(s[i+1:]))
+			if s[i] == '\\':
+				i += 1
+				if i >= len(s):
+					raise ValueError('unterminated quoted-pair')
+			b.append(s[i])
+			i += 1
+		else:
+			raise ValueError('unterminated string')
+	elif s[0] in LONERS:
+		return (s[0], *trim(s[1:]))
+	else:
+		i = 0
+		while s[i] not in LONERS | SPACES:
+			i += 1
+		return (str(s[:i]), *trim(s[i:]))
+
+def tokenize(s):
+	w, s = trim(s)
+	yield None, w
+	while s:
+		t, w, s = chop(s)
+		yield t, w
+
+def join(ps):
+	return ''.join((t or '') + (' ' if w else '') for t, w in ps)
+
+if __name__ == '__main__':
+	from sys import stdin
+	msg = Parser().parse(stdin, headersonly=True)
+	print(join(tokenize(strview(msg['Present']))))
