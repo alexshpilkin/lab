9a735220a26ed8e3b90db07d7f60998483cfbe6a
Alexander Shpilkin <ashpilkin@gmail.com>
2019-05-07 08:42:06 +0300

    Use weight argument for 2D histogram as well

--- basic.py
+++ basic.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser()
 parser.add_argument('--ruelectiondata_json', default='https://github.com/schitaytesami/data/releases/download/20180909/lab_20180909.json')
 parser.add_argument('--kobak_npz', default='https://github.com/schitaytesami/lab/releases/download/data/electionsData.npz')
-parser.add_argument('--kobak_weights', default='voters', choices=['voters', 'given', 'leader', 'off'], help='''  'off'     (counts polling stations);   'voters'  (counts registered voters);  'given'   (counts given ballots);  'leader'  (counts ballots for the leader) ''')
+parser.add_argument('--weights', default='voters', choices=['voters', 'given', 'leader', 'off'], help='''  'off'     (counts polling stations);   'voters'  (counts registered voters);  'given'   (counts given ballots);  'leader'  (counts ballots for the leader) ''')
 parser.add_argument('--kobak_minsize', default=0, type=int)
 parser.add_argument('--kobak_addnoise', action='store_true', help='Whether to add add U(-0.5,0.5) noise to the numerators (to remove division artifacts)')
 parser.add_argument('--seed', default=1, type=int)
@@ -44,6 +44,13 @@ year = 2018
 D = load_data(args.kobak_npz, year=year)
 locals().update({k : D[k] for k in D.dtype.names})
 
+wval, wlbl = {
+  'voters': (voters_registered, 'Voters'),
+  'given':  (voters_voted, 'Ballots given'),
+  'leader': (leader, 'Ballots for leader'),
+  'off':    (np.ones(voters_registered.shape), 'Polling stations'),
+}.get(args.weights, None)
+
 # Settings used in our papers:
 # * AOAS-2016:         binwidth=0.1,  addNoise=False, weights='voters', minSize = 0
 # * Significance-2016: binwidth=0.25, addNoise=True,  weights='off'     minSize = 0
@@ -57,21 +64,20 @@ ind = (ballots_valid_invalid > 0) & (voters_voted < voters_registered) & (voters
 edges = np.arange(-binwidth/2, 100 + binwidth/2, binwidth)
 centers = np.arange(0, 100, binwidth)
 noise = np.zeros(np.sum(ind)) if not args.kobak_addnoise else np.random.rand(np.sum(ind)) - .5
-w = dict(voters=voters_registered, given=voters_voted, leader=leader)[args.kobak_weights][ind] if args.kobak_weights != 'off' else None
+w = wval[ind]
 h1 = np.histogram(100 * (voters_voted[ind] + noise) / voters_registered[ind], bins=edges, weights=w)[0]
 h2 = np.histogram(100 * (leader[ind] + noise) / ballots_valid_invalid[ind], bins=edges, weights=w)[0]
-ylbl = dict(voters='Voters', given='Ballots given', leader='Ballots for leader').get(args.kobak_weights, 'Polling stations')
 plt.figure(figsize=(9,6))
 plt.subplot(211)
 plt.plot(centers, h1, linewidth=1)
 plt.xlabel("Turnout (%)")
-plt.ylabel(f'{ylbl} in {binwidth}% bins')
+plt.ylabel(f'{wlbl} in {binwidth}% bins')
 plt.xticks(np.arange(0, 101, 10))
 plt.title(f'Russian election {year}')
 plt.subplot(212)
 plt.plot(centers, h2, linewidth=1)
 plt.xlabel("Leader's result (%)")
-plt.ylabel(f'{ylbl} in {binwidth}% bins')
+plt.ylabel(f'{wlbl} in {binwidth}% bins')
 plt.xticks(np.arange(0, 101, 10))
 plt.tight_layout()
 plt.savefig('basic1.png')
@@ -85,12 +91,12 @@ edges = np.arange(-binwidth/2, 100 + binwidth/2, binwidth)
 centers = np.arange(0, 100, binwidth)
 plt.figure(figsize=(3,3))
 ind = (ballots_valid_invalid > 0) & (voters_voted < voters_registered) & (voters_registered >= args.kobak_minsize) & np.array(['Зарубеж' not in s and 'за пределами' not in s for s in regions])
-h = np.histogram2d(100 * voters_voted[ind] / voters_registered[ind], 100 * leader[ind] / ballots_valid_invalid[ind], bins=edges, weights=voters_registered[ind])[0]
-plt.imshow(h.T, vmin=0, vmax=50000, origin='lower', extent=[0,100,0,100], cmap='viridis', interpolation='none')
+h = np.histogram2d(100 * voters_voted[ind] / voters_registered[ind], 100 * leader[ind] / ballots_valid_invalid[ind], bins=edges, weights=wval[ind])[0]
+plt.imshow(h.T, vmin=0, vmax=np.quantile(h, 0.99), origin='lower', extent=[0,100,0,100], cmap='viridis', interpolation='none')
 plt.xticks([])
 plt.yticks([])
 plt.axis('off')
-plt.text(10,85,year, color='w')
+plt.text(10, 85, year, color='w')
 plt.tight_layout()
 plt.savefig('basic2.png')
 plt.close()
