a7eab8d9e31a83dd15cbdb1ee89f86976dfc3a75
Alexander Shpilkin <ashpilkin@gmail.com>
2019-05-08 13:23:24 +0300

    Add ticks and labels

--- bubbles.py
+++ bubbles.py
@@ -22,7 +22,7 @@ def rlencode(inarray):  # Run-length encoding, <https://stackoverflow.com/a/3268
 
 def plot(D, region):
 	idx = D.region == region
-	tlen, tpos, terr = rlencode(D.territory[idx])
+	tlen, tidx, terr = rlencode(D.territory[idx])
 	tsum = np.insert(np.cumsum(tlen), 0, 0)
 	assert np.unique(terr).shape == terr.shape
 
@@ -31,12 +31,25 @@ def plot(D, region):
 	            100 * D.leader[idx] / D.ballots_valid_invalid[idx],
 	            s=D.voters_registered[idx] / np.quantile(D.voters_registered, 0.5) * 20,
 	            alpha=0.5)
-	plt.title(election_data.translit(region))
+	plt.title(election_data.translit(region) + '\n', size=20)
 
 	plt.xlabel('Precinct')
 	for x in tsum:
 		plt.axvline(x, 0, 1, color='black', alpha=0.25, linewidth=1)
-	plt.xticks([])
+	ax1 = plt.gca()
+	ax1.set_xlim((0, np.count_nonzero(idx)))
+	ax1.set_xlabel('Precinct')
+	ax1.set_xticks(tsum[:-1])
+	ax1.set_xticklabels(D.precinct[idx][tidx], ha='center', rotation=90)
+	ax2 = ax1.twiny()
+	ax2.set_xlim(ax1.get_xlim())
+	ax2.set_xlabel('Territory')
+	ax2.set_xticks(tsum[:-1])
+	ax2.set_xticklabels(map(election_data.translit, terr),
+	                    ha='left', rotation=60)
+	ax2.tick_params(axis='x', rotation=60,
+	                bottom=False, top=True,
+	                labelbottom=False, labeltop=True)
 
 	plt.ylabel('Leader’s result')
 	plt.ylim(0, 100)
