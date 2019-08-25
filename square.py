f2cff60e7fd18dd4564b6ca6441bc0819eb7eec4
Vadim Kantorov <vadimkantorov@gmail.com>
2019-08-25 17:05:01 +0200

    ...

--- square.py
+++ square.py
@@ -30,38 +30,38 @@ def histogram(D, *, binwidth, weights='voters', minsize=0, noise=False, seed=1):
 	                   bins=edges, weights=wval)[0]
 	return wlbl, centers, h
 
-def plot(D, title, cmap='viridis', binwidth=0.25, **kwargs):
+def plot(D, title, cmap='viridis', binwidth=0.25, aspect = 3, spacing = 0.15, **kwargs):
 	wlbl, centers, h = histogram(D, binwidth=binwidth, **kwargs)
 	ht = np.sum(h, axis=1)
 	hr = np.sum(h, axis=0)
 
 	ylog = int(np.ceil(np.log10(min(np.max(ht), np.max(hr))))) - 1
 
-	aspect, spacing = 3, 0.15
 	plt.suptitle(title, size=20, y=0.925, va='baseline')
 
 	axs = plt.gcf().subplots(2, 2, sharex='col', sharey='row', gridspec_kw=dict(width_ratios=[aspect, 1], wspace=spacing, height_ratios=[1, aspect], hspace=spacing))
 	ax = axs[0,1]
 	ax.text(0.5, 0.5, f'$\\times 10^{{{ylog}}}$ {wlbl}\nin ${binwidth}\\,\\%$ bin', wrap=True, ha='center', va='center', transform=ax.transAxes)	# the \n is a hack to force good wrapping
-	ax.tick_params(right=False, top=False, left=True, bottom=True, labelright=False, labeltop=False, labelleft=True, labelbottom=True)
+	ax.set_frame_on(False)
+	ax.axis('off')
 
 	ax = axs[0,0]
 	ax.plot(centers, ht / (10 ** ylog), linewidth=1, color=plt.get_cmap(cmap)(0))
 	ax.set_xticks(np.arange(0, 101, 10))
 	ax.set_ylim(bottom=0)
-	ax.tick_params(right=True, top=False, left=False, bottom=True, labelright=False, labeltop=False, labelleft=False, labelbottom=True)
+	ax.tick_params(right=True, top=False, left=False, bottom=True, labelright=True, labeltop=False, labelleft=False, labelbottom=True)
 
 	ax = axs[1,1]
 	ax.plot(hr / (10 ** ylog), centers, linewidth=1, color=plt.get_cmap(cmap)(0))
 	ax.set_xlim(left=0)
 	ax.set_yticks(np.arange(0, 101, 10))
-	ax.tick_params(right=False, top=True, left=True, bottom=False, labelright=False, labeltop=False, labelleft=True, labelbottom=False)
+	ax.tick_params(right=False, top=True, left=True, bottom=False, labelright=False, labeltop=True, labelleft=True, labelbottom=False)
 
 	ax = axs[1,0]
 	ax.imshow(h.T, vmin=0, vmax=np.quantile(h[h>0], 0.95), origin='lower', extent=[0,100,0,100], cmap=cmap, interpolation='none')
 	ax.set_xlabel('Turnout %')
 	ax.set_ylabel('Leader’s result %')
-	ax.tick_params(right=True, top=True, left=False, bottom=False, labelright=False, labeltop=False, labelleft=False, labelbottom=False)
+	ax.axis('off')
 
 if __name__ == '__main__':
 	import os
