dc51342e9929b7187cd747609098eadd5efd5a3c
Vadim Kantorov <vadimkantorov@gmail.com>
2019-08-25 16:11:22 +0200

    square.py simplification

--- square.py
+++ square.py
@@ -10,7 +10,7 @@ import election_data
 # * Significance-2016: binwidth=0.25, addNoise=True,	weights='ones',	 minsize = 0
 # * Significance-2018: binwidth=0.1,	addNoise=True,	weights='ones',	 minsize = 0
 
-def histogram(D, *, binwidth=0.25, weights='voters', minsize=0, noise=False, seed=1):
+def histogram(D, *, binwidth, weights='voters', minsize=0, noise=False, seed=1):
 	rnd = np.random.RandomState(seed)
 	edges = np.arange(-binwidth/2, 100 + binwidth/2, binwidth)
 	centers = np.arange(0, 100, binwidth)
@@ -30,48 +30,38 @@ def histogram(D, *, binwidth=0.25, weights='voters', minsize=0, noise=False, see
 	                   bins=edges, weights=wval)[0]
 	return wlbl, centers, h
 
-def plot(D, title, cmap='viridis', **kwargs):
-	wlbl, centers, h = histogram(D, **kwargs)
-	binwidth = centers[1] - centers[0]
+def plot(D, title, cmap='viridis', binwidth=0.25, **kwargs):
+	wlbl, centers, h = histogram(D, binwidth=binwidth, **kwargs)
 	ht = np.sum(h, axis=1)
 	hr = np.sum(h, axis=0)
 
-	ymax = min(np.max(ht), np.max(hr))
-	ylog, yfac = 0, 1
-	while yfac < ymax:
-		ylog, yfac = ylog+1, yfac*10
-	ylog, yfac = ylog-1, yfac//10
+	ylog = int(np.ceil(np.log10(min(np.max(ht), np.max(hr))))) - 1
 
 	aspect, spacing = 3, 0.15
-
 	plt.suptitle(title, size=20, y=0.925, va='baseline')
-	axs = plt.gcf().subplots(2, 2, sharex='col', sharey='row', gridspec_kw=dict(width_ratios=[aspect, 1], wspace=spacing, height_ratios=[1, aspect], hspace=spacing))
 
+	#plt.subplot(222, frameon=False)
+	#plt.text(0.5, 0.5, f'$\\times 10^{{{ylog}}}$ {wlbl}\nin ${binwidth}\\,\\%$ bin', wrap=True, ha='center', va='center')
+	#ax.tick_params(right=False, top=False, left=True, bottom=True,
+	#               labelright=False, labeltop=False, labelleft=True, labelbottom=True)
+
+	axs = plt.gcf().subplots(2, 2, sharex='col', sharey='row', gridspec_kw=dict(width_ratios=[aspect, 1], wspace=spacing, height_ratios=[1, aspect], hspace=spacing))
 	ax = axs[0,1]
 	ax.text(0.5, 0.5, f'$\\times 10^{{{ylog}}}$ {wlbl}\nin ${binwidth}\\,\\%$ bin', wrap=True, ha='center', va='center', transform=ax.transAxes)	# the \n is a hack to force good wrapping
-	ax.set_frame_on(False)
-	ax.axhline(0, 0, 1, color='black')
-	ax.axvline(0, 0, 1, color='black')
 	ax.tick_params(right=False, top=False, left=True, bottom=True,
 	               labelright=False, labeltop=False, labelleft=True, labelbottom=True)
 
 	ax = axs[0,0]
-	ax.plot(centers, ht / yfac, linewidth=1, color=plt.get_cmap(cmap)(0))
+	ax.plot(centers, ht / (10 ** ylog), linewidth=1, color=plt.get_cmap(cmap)(0))
 	ax.set_xticks(np.arange(0, 101, 10))
-	ax.set_ylim(0, ax.get_ylim()[1])
-	ax.set_frame_on(False)
-	ax.axhline(0, 0, 1, color='black')
-	ax.axvline(100, 0, 1, color='black')
+	ax.set_ylim(bottom=0)
 	ax.tick_params(right=True, top=False, left=False, bottom=True,
 	               labelright=False, labeltop=False, labelleft=False, labelbottom=True)
 
 	ax = axs[1,1]
-	ax.plot(hr / yfac, centers, linewidth=1, color=plt.get_cmap(cmap)(0))
+	ax.plot(hr / (10 ** ylog), centers, linewidth=1, color=plt.get_cmap(cmap)(0))
 	ax.set_xlim(0, ax.get_xlim()[1])
 	ax.set_yticks(np.arange(0, 101, 10))
-	ax.set_frame_on(False)
-	ax.axhline(100, 0, 1, color='black')
-	ax.axvline(0, 0, 1, color='black')
 	ax.tick_params(right=False, top=True, left=True, bottom=False,
 	               labelright=False, labeltop=False, labelleft=True, labelbottom=False)
 
@@ -79,29 +69,8 @@ def plot(D, title, cmap='viridis', **kwargs):
 	ax.imshow(h.T, vmin=0, vmax=np.quantile(h[h>0], 0.95), origin='lower', extent=[0,100,0,100], cmap=cmap, interpolation='none')
 	ax.set_xlabel('Turnout %')
 	ax.set_ylabel('Leader’s result %')
-	ax.set_frame_on(False)
-	ax.axhline(100, 0, 1, color='black')
-	ax.axvline(100, 0, 1, color='black')
 	ax.tick_params(right=True, top=True, left=False, bottom=False,
 								 labelright=False, labeltop=False, labelleft=False, labelbottom=False)
-
-	top    = min(axs[0,0].get_position().y0, axs[0,0].get_position().y1)
-	bottom = max(axs[1,0].get_position().y0, axs[1,0].get_position().y1)
-	offset = ((top - bottom) / axs[0,0].get_position().height / 2 -
-	          axs[0,0].xaxis.get_major_ticks()[0].get_tick_padding() / 144)
-	for tick in axs[0,0].xaxis.get_major_ticks() + axs[0,1].xaxis.get_major_ticks():
-		tick.label1.set_va('center')
-		tick.set_pad(0)
-		tick.label1.set_position((tick.label1.get_position()[0], -offset))
-
-	right = min(axs[1,1].get_position().x0, axs[1,1].get_position().x1)
-	left  = max(axs[1,0].get_position().x0, axs[1,0].get_position().x1)
-	offset = ((right - left) / axs[1,1].get_position().width / 2 -
-	          axs[1,1].yaxis.get_major_ticks()[0].get_tick_padding() / 144)
-	for tick in axs[0,1].yaxis.get_major_ticks() + axs[1,1].yaxis.get_major_ticks():
-		tick.label1.set_ha('center')
-		tick.set_pad(0)
-		tick.label1.set_position((-offset, tick.label1.get_position()[1]))
 
 
 if __name__ == '__main__':
@@ -111,7 +80,7 @@ if __name__ == '__main__':
 	matplotlib.use('Agg')
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('data', nargs='?', metavar='DATA', default='https://github.com/schitaytesami/lab/releases/download/data-v2/2018.tsv.gz', help='Data file to use')
+	parser.add_argument('data', nargs='?', metavar='DATA', default='https://github.com/schitaytesami/lab/releases/download/data-v2/RU_2018-03-18_president.tsv.gz', help='Data file to use')
 	parser.add_argument('--bin-width', default=0.25, type=float, help='Bin width in percentage points')
 	parser.add_argument('--weights', default='voters', choices={'voters', 'given', 'leader', 'ones'}, help="'ones' (counts polling stations), 'voters' (counts registered voters), 'given' (counts ballots given), or 'leader' (counts ballots for the leader)")
 	parser.add_argument('--min-size', default=0, type=int, help='Minimum precinct size to include')
